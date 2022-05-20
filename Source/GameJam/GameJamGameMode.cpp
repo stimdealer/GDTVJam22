f4818ab09cb4198669c76cd5635ea77fc7ea32d3
stimdealer <stimdealer0001@gmail.com>
2022-05-19 17:36:18 -0700

    na
    na

--- Source/GameJam/GameJamGameMode.cpp
+++ Source/GameJam/GameJamGameMode.cpp
@@ -6,10 +6,5 @@
 
 AGameJamGameMode::AGameJamGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	DefaultPawnClass = nullptr;
 }
